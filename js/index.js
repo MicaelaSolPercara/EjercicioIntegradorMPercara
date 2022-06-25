8e77a6974af30b10c3b4c6280d2e9f866a1702e3
Micaela Sol Percara <micpercara@gmail.com>
2022-06-25 00:14:35 -0300

    Correccion1

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
-const form=document.getElementById ('form');
-const parrafo=document.getElementById ('warnings');
-let inputNombre=document.getElementById ('nombre');
-let inputOpinion=document.getElementById ('opinion');
-let btnEnviar=document.getElementById ('btnEnviar');
+
+$("ul").click(function(ev){
+    ev.stopPropagation();
+  })
+
+
+const form = document.getElementById ('form');
+const parrafo = document.getElementById ('warnings');
+let inputNombre = document.getElementById ('nombre');
+let inputOpinion = document.getElementById ('opinion');
+let btnEnviar = document.getElementById ('btnEnviar');
 
 form.addEventListener ('submit', e=> {
     e.preventDefault ();
-    let warnings="";
-    let entrar=false
-    parrafo.innerHTML =""
+    let warnings = "";
+    let entrar = false;
+    parrafo.innerHTML = ""
  
-if (inputNombre.value.length<3); {
- warnings+= `¡El nombre es demasiado corto!`;
- entrar=true; 
-}
-if (inputOpinion.value.length < 5); {
-warnings+= `¡Escriba una opinión de al menos 15 caracteres! <br>`;
- entrar=true;
- }
- if (entrar) {
+if (inputNombre.value.length < 3) {
+ warnings += ('¡El nombre es demasiado corto!');
+ entrar = true; 
+} else if (inputOpinion.value.length < 5) {
+ warnings += ('¡Escriba una opinión de al menos 15 caracteres!');
+ entrar = true;
+} else if (entrar) {
  parrafo.innerHTML = warnings
+} else {
+  parrafo.innerHTML = Enviado
 }
-else {parrafo.innerHTML=Enviado}
-
 });
      
      
